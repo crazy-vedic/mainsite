7eef667e4a4735222c67368f4aab62fbe964a561
Vedic Laptop <vedic20052005@gmail.com>
2023-07-08 18:21:51 +0530

    rr

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const backendPort = 3001; // Port for running the backend server
 app.use(express.static(path.join(__dirname, 'studentManagement-frontend', 'build')));
 
 // Define additional routes or middleware if needed
-app.use('/api', backend);
+//app.use('/api', backend);
 
 // Serve the index.html file for all other routes
 app.get('*', (req, res) => {
