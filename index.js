36aad9ffd2fc0e56a30b65699eebcea1307de198
Vedic Laptop <vedic20052005@gmail.com>
2023-07-08 18:39:44 +0530

    gpt help

--- index.js
+++ index.js
@@ -4,35 +4,40 @@ const backend = require('./studentManagement-backend/index.js');
 const mongoose = require('mongoose');
 const http = require('http');
 
-const app = express();
+const frontendApp = express();
+const backendApp = express();
+
 const frontendPort = 80; // Port for serving the frontend build
 const backendPort = 3001; // Port for running the backend server
 
 // Serve static files from the 'build' folder
-app.use(express.static(path.join(__dirname, 'studentManagement-frontend', 'build')));
+frontendApp.use(express.static(path.join(__dirname, 'studentManagement-frontend', 'build')));
 
-// Mount the backend middleware
-app.use('/api', backend);
-
-// Serve the index.html file for all other routes
-app.get('*', (req, res) => {
+// Serve the index.html file for all frontend routes
+frontendApp.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'studentManagement-frontend', 'build', 'index.html'));
 });
 
-// Create the HTTP server instance
-const server = http.createServer(app);
+// Mount the backend middleware
+backendApp.use('/api', backend);
 
-// Start the server for serving the frontend build and running the backend server
+// Create the frontend and backend server instances
+const frontendServer = http.createServer(frontendApp);
+const backendServer = http.createServer(backendApp);
+
+// Start the frontend server
+frontendServer.listen(frontendPort, () => {
+  console.log(`Frontend server is running on port ${frontendPort}`);
+});
+
+// Start the backend server
 const mongoURL = 'mongodb+srv://admin:pass@cluster0.tjfctuy.mongodb.net/studentDBMSDB?retryWrites=true&w=majority';
 mongoose.connect(mongoURL, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
 }).then(() => {
   console.log('Connected to database studentDBMSDB');
-  server.listen(frontendPort, () => {
-    console.log(`Server is running on port ${frontendPort}`);
-    backend.listen(backendPort, () => {
-      console.log(`Backend server is running on port ${backendPort}`);
-    });
+  backendServer.listen(backendPort, () => {
+    console.log(`Backend server is running on port ${backendPort}`);
   });
 });
