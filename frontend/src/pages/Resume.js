a6d4385562b9efe3ac085083c3a2b5a602846a2a
Vedic Laptop <vedic20052005@gmail.com>
2024-11-05 21:06:04 +0530

    minimized no. of packages

--- frontend/src/pages/Resume.js
+++ frontend/src/pages/Resume.js
@@ -2,12 +2,19 @@ import React from 'react';
 import resume from './resume.pdf';
 
 function Resume() {
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+  if (isMobile) {
+    window.location.href = resume;
+    return null;
+  }
+
   return (
     <div style={{position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', overflow: 'hidden'}}>
-      <embed 
+      <iframe 
         src={resume}
         type="application/pdf" 
-        style={{width: '100%', height: '100%'}}
+        style={{width: '100%', height: '100%', border: 'none'}}
       />
     </div>
   );
